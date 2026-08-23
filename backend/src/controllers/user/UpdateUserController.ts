9e119b77c206e29d26afb0902bc609483012b127
Adrian Barros <adrianbarros.2910@gmail.com>
2026-08-22 21:23:40 -0300

    dash-top-category

--- backend/src/controllers/user/UpdateUserController.ts
+++ backend/src/controllers/user/UpdateUserController.ts
@@ -4,11 +4,11 @@ import { UpdateUserService } from '../../services/user/UpdateUserService.js';
 export class UpdateUserController {
   async handle(req: Request, res: Response) {
     const id = req.user_id;
-    const { name, email, salary } = req.body;
+    const { name, email, phone, salary } = req.body;
 
     const updateUserService = new UpdateUserService();
     
-    const user = await updateUserService.execute({ user_id: id, name, salary, email });
+    const user = await updateUserService.execute({ user_id: id, name, salary, email, phone });
 
     return res.json(user);
   }
